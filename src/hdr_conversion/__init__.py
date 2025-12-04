81fad65607836f3cb8d163511e7a5e166f6c13b2
Jackchou00 <jackchou00@zju.edu.cn>
2025-12-04 22:03:36 +0800

    Switch to src layout. - Removed unused utility files related to EOTF inverse calculations.

---
+++ src/hdr_conversion/__init__.py
@@ -0,0 +1,20 @@
+"""
+HDR Conversion Library
+
+A library for converting Apple HEIC HDR images to AVIF format.
+"""
+
+# Core functions for Apple HEIC processing
+from .apple_heic.color_conversion import apply_gain_map
+from .apple_heic.get_images import read_base_and_gain_map
+from .apple_heic.headroom import get_headroom
+
+# Core functions for image output
+from .img_output.pqavif import save_np_array_to_avif
+
+__all__ = [
+    "apply_gain_map",
+    "read_base_and_gain_map", 
+    "get_headroom",
+    "save_np_array_to_avif"
+]
